7720f25eaa2f2731bdd36f66b9d9c189d2820ad0
Oleg Zasukha <ozamericos@gmail.com>
2023-06-19 03:51:24 +0000

    Array_Set_ContainsDuplicates

--- src/main/java/ContainsDuplicates.java
+++ src/main/java/ContainsDuplicates.java
@@ -1,5 +1,5 @@
-
-
+import java.util.HashSet;
+import java.util.Set;
 public class ContainsDuplicates {
     /**
      * Determine if an array of items contains any duplicate values. You should use a Set for this: remember that
@@ -7,7 +7,14 @@ public class ContainsDuplicates {
      * @param nums an array of ints.
      * @return true if nums contains any duplicate values, false if it does not.
      */
-    public boolean containsDuplicate(int[] nums){
-        return false;
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+        if (set.contains(num)) {
+            return true;
+        }
+        set.add(num);
     }
-}
+
+        return false;
+}}
